e09c50ad263788b0e20a4faedc32fca1d972adbc
Daveny Kuria <davenykuria@gmail.com>
2026-05-13 23:21:24 +0300

    fix(script): ensure code blocks only run on their respective pages

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
 // Getting last visited site
-console.log('script running on:', window.location.pathname);
+let currentPage = window.location.pathname;
+console.log('script running on:', currentPage);
 const lastVisited = localStorage.getItem('currentPage');
 localStorage.setItem('currentPage', window.location.pathname);
 console.log(lastVisited);
 
 // checking if submit button is pressed
-const donationForm = document.querySelector(".donation-form");
-const thanksMessage = document.querySelector(".thanks-message");
 
-function onSubmit(){
-    event.preventDefault();
-    donationForm.classList.add("hidden");
-    thanksMessage.innerHTML = `
-    <h3>Thanks for your interest!</h3>
-    <h3>A list of organisations you can donate to relative to your choices has been mailed to you!</h3>`;
+if (currentPage === "/donations.html"){
+    const donationForm = document.querySelector(".donation-form");
+    const thanksMessage = document.querySelector(".thanks-message");
+
+    function onSubmit(){
+        event.preventDefault();
+        donationForm.classList.add("hidden");
+        thanksMessage.innerHTML = `
+        <h3>Thanks for your interest!</h3>
+        <h3>A list of organisations you can donate to relative to your choices has been mailed to you!</h3>`;
+    }
+
+    donationForm.addEventListener('submit', onSubmit);
 }
-
-donationForm.addEventListener('submit', onSubmit);
 // quotes
 
-let quotes = [
-    "In all things of nature, there is something of the marvelous.",
-    "Colors are the smiles of nature.",
-    "The butterfly counts not months but moments, and has time enough.",
-    "My soul steers me into nature's silence.",
-    "Nature does not hurry, yet everything is accomplished.",
-    "Every flower is a soul blossoming in nature."
-]
+if (currentPage === "/index.html"){
+    let quotes = [
+        "In all things of nature, there is something of the marvelous.",
+        "Colors are the smiles of nature.",
+        "The butterfly counts not months but moments, and has time enough.",
+        "My soul steers me into nature's silence.",
+        "Nature does not hurry, yet everything is accomplished.",
+        "Every flower is a soul blossoming in nature."
+    ]
 
-let quoteAuthors = [
-    "Aristotle",
-    "Leigh Hunt",
-    "Rabindranath Tagore",
-    "Angie Weiland-Crosby",
-    "Lao Tzu",
-    "Gerard De Nerval"
-]
+    let quoteAuthors = [
+        "Aristotle",
+        "Leigh Hunt",
+        "Rabindranath Tagore",
+        "Angie Weiland-Crosby",
+        "Lao Tzu",
+        "Gerard De Nerval"
+    ]
 
-// Cycle
+    // Cycle
 
-let currentIndex = 0;
-const quoteSection = document.querySelector(".quote-section");
+    let currentIndex = 0;
+    const quoteSection = document.querySelector(".quote-section");
 
-function quoteUpdate(){
-    quoteSection.classList.add("fade-out");
-    
-    setTimeout(() => {
-        quoteSection.innerHTML = `
-            <p class="quote-text">${quotes[currentIndex]}</p>
-            <p class="quote-author">~${quoteAuthors[currentIndex]}~</p>
-        `;
-
-        quoteSection.classList.remove("fade-out");
-        if (currentIndex === quotes.length - 1){
-            currentIndex = 0;
-        } else{
-            currentIndex++;
-        }
+    function quoteUpdate(){
+        quoteSection.classList.add("fade-out");
         
-    }, 500);
-}
+        setTimeout(() => {
+            quoteSection.innerHTML = `
+                <p class="quote-text">${quotes[currentIndex]}</p>
+                <p class="quote-author">~${quoteAuthors[currentIndex]}~</p>
+            `;
 
-quoteUpdate();
-setInterval(quoteUpdate, 4000);
+            quoteSection.classList.remove("fade-out");
+            if (currentIndex === quotes.length - 1){
+                currentIndex = 0;
+            } else{
+                currentIndex++;
+            }
+            
+        }, 500);
+    }
+
+    quoteUpdate();
+    setInterval(quoteUpdate, 4000);
+}
