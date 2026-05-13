ff315fc16e1e5fe0b08ed7f783a4374340d290ee
Daveny Kuria <davenykuria@gmail.com>
2026-05-13 17:29:28 +0300

    style(quotes): refine quote styling

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // quotes
 
 let quotes = [
-    "'In all things of nature, there is something of the marvelous'",
-    "'Colors are the smiles of nature.'",
-    "'The butterfly counts not months but moments, and has time enough.'",
-    "'My soul steers me into nature's silence.'",
-    "'Nature does not hurry, yet everything is accomplished.'",
-    "'Every flower is a soul blossoming in nature.'"
+    "In all things of nature, there is something of the marvelous.",
+    "Colors are the smiles of nature.",
+    "The butterfly counts not months but moments, and has time enough.",
+    "My soul steers me into nature's silence.",
+    "Nature does not hurry, yet everything is accomplished.",
+    "Every flower is a soul blossoming in nature."
 ]
 
 let quoteAuthors = [
