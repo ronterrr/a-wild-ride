6349cbde47bdb6ea0d2778e3073f79e3e9e5e945
Daveny Kuria <davenykuria@gmail.com>
2026-05-13 17:04:24 +0300

    script(index): add quotes and quoteAuthors to script

---
+++ script.js
@@ -0,0 +1,19 @@
+// quotes
+
+let quotes = [
+    "'In all things of nature, there is something of the marvelous'",
+    "'Colors are the smiles of nature.'",
+    "'The butterfly counts not months but moments, and has time enough.'",
+    "'My soul steers me into nature's silence.'",
+    "'Nature does not hurry, yet everything is accomplished.'",
+    "'Every flower is a soul blossoming in nature.'"
+]
+
+let quoteAuthors = [
+    "Aristotle",
+    "Leigh Hunt",
+    "Rabindranath Tagore",
+    "Angie Weiland-Crosby",
+    "Lao Tzu",
+    "Gerard De Nerval"
+]
